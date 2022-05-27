81acdb4abed8dc82bf5b4889f51589b1a20b7868
seondongpyo <seondongpyo@gmail.com>
2022-05-27 21:55:22 +0900

    refactor: 테스트 픽스처 생성 메서드 분리

--- src/test/java/me/seondongpyo/videoshop/customer/ui/CustomerRestControllerTest.java
+++ src/test/java/me/seondongpyo/videoshop/customer/ui/CustomerRestControllerTest.java
@@ -9,11 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.UUID;
@@ -40,9 +37,7 @@ class CustomerRestControllerTest {
     @DisplayName("새로운 고객을 등록한다.")
     @Test
     void create() throws Exception {
-        Customer customer = new Customer();
-        customer.setId(UUID.randomUUID());
-        customer.setMembershipId(UUID.randomUUID());
+        Customer customer = customer();
 
         mvc.perform(post("/customers")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -53,10 +48,7 @@ class CustomerRestControllerTest {
     @DisplayName("특정 고객을 조회한다.")
     @Test
     void findById() throws Exception {
-        Customer customer = new Customer();
-        customer.setId(UUID.randomUUID());
-        customer.setMembershipId(UUID.randomUUID());
-        Customer created = customerService.create(customer);
+        Customer created = customerService.create(customer());
 
         mvc.perform(get("/customers/{id}", created.getId()))
             .andExpect(status().isOk())
@@ -66,14 +58,9 @@ class CustomerRestControllerTest {
     @DisplayName("고객 목록을 조회한다.")
     @Test
     void findAll() throws Exception {
-        Customer customer1 = new Customer();
-        customer1.setId(UUID.randomUUID());
-        customer1.setMembershipId(UUID.randomUUID());
+        Customer customer1 = customer();
+        Customer customer2 = customer();
         customerService.create(customer1);
-
-        Customer customer2 = new Customer();
-        customer2.setId(UUID.randomUUID());
-        customer2.setMembershipId(UUID.randomUUID());
         customerService.create(customer2);
 
         List<Customer> customers = List.of(customer1, customer2);
@@ -81,5 +68,12 @@ class CustomerRestControllerTest {
         mvc.perform(get("/customers"))
             .andExpect(status().isOk())
             .andExpect(content().json(objectMapper.writeValueAsString(customers)));
+    }
+
+    private Customer customer() {
+        Customer customer = new Customer();
+        customer.setId(UUID.randomUUID());
+        customer.setMembershipId(UUID.randomUUID());
+        return customer;
     }
 }
