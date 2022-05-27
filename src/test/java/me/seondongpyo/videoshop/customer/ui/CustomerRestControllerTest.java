579e05de48c02b382696d198821f94aeded0a023
seondongpyo <seondongpyo@gmail.com>
2022-05-27 21:05:31 +0900

    test: 특정 고객을 조회한다

--- src/test/java/me/seondongpyo/videoshop/customer/ui/CustomerRestControllerTest.java
+++ src/test/java/me/seondongpyo/videoshop/customer/ui/CustomerRestControllerTest.java
@@ -1,6 +1,7 @@
 package me.seondongpyo.videoshop.customer.ui;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.seondongpyo.videoshop.customer.application.CustomerService;
 import me.seondongpyo.videoshop.customer.domain.Customer;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,13 +9,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
 import java.util.UUID;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -25,6 +31,9 @@ class CustomerRestControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Autowired
+    private CustomerService customerService;
 
     @DisplayName("새로운 고객을 등록한다.")
     @Test
@@ -39,4 +48,16 @@ class CustomerRestControllerTest {
             .andExpect(status().isCreated());
     }
 
+    @DisplayName("특정 고객을 조회한다.")
+    @Test
+    void findById() throws Exception {
+        Customer customer = new Customer();
+        customer.setId(UUID.randomUUID());
+        customer.setMembershipId(UUID.randomUUID());
+        Customer created = customerService.create(customer);
+
+        mvc.perform(get("/customers/{id}", created.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().json(objectMapper.writeValueAsString(created)));
+    }
 }
