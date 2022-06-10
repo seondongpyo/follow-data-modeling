3bfd80698cbcfd06b560217258a75cbffdee9d7c
seondongpyo <seondongpyo@gmail.com>
2022-06-10 16:03:20 +0900

    refactor: `@WebMvcTest`, `@MockBean` 등으로 테스트 리팩토링

--- src/test/java/me/seondongpyo/videoshop/customer/ui/CustomerRestControllerTest.java
+++ src/test/java/me/seondongpyo/videoshop/customer/ui/CustomerRestControllerTest.java
@@ -3,72 +3,70 @@ package me.seondongpyo.videoshop.customer.ui;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.seondongpyo.videoshop.customer.application.CustomerService;
 import me.seondongpyo.videoshop.customer.domain.Customer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@Transactional
-@AutoConfigureMockMvc
+@WebMvcTest(CustomerRestController.class)
 class CustomerRestControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Autowired
+    @MockBean
     private CustomerService customerService;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private final Customer customer1 = customer();
+    private final Customer customer2 = customer();
+
+    @BeforeEach
+    void setup() {
+        given(customerService.create(any(Customer.class))).willReturn(customer1);
+        given(customerService.findById(any(UUID.class))).willReturn(customer1);
+        given(customerService.findAll()).willReturn(List.of(customer1, customer2));
+    }
 
     @DisplayName("새로운 고객을 등록한다.")
     @Test
     void create() throws Exception {
-        Customer customer = customer();
-
         mvc.perform(post("/customers")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(customer)))
+                .content(objectMapper.writeValueAsString(customer1)))
             .andExpect(status().isCreated())
-            .andExpect(content().json(objectMapper.writeValueAsString(customer)));
+            .andExpect(content().json(objectMapper.writeValueAsString(customer1)));
     }
 
     @DisplayName("특정 고객을 조회한다.")
     @Test
     void findById() throws Exception {
-        Customer created = customerService.create(customer());
-
-        mvc.perform(get("/customers/{id}", created.getId()))
+        mvc.perform(get("/customers/{id}", customer1.getId()))
             .andExpect(status().isOk())
-            .andExpect(content().json(objectMapper.writeValueAsString(created)));
+            .andExpect(content().json(objectMapper.writeValueAsString(customer1)));
     }
 
     @DisplayName("고객 목록을 조회한다.")
     @Test
     void findAll() throws Exception {
-        Customer customer1 = customer();
-        Customer customer2 = customer();
-        customerService.create(customer1);
-        customerService.create(customer2);
-
-        List<Customer> customers = List.of(customer1, customer2);
-
         mvc.perform(get("/customers"))
             .andExpect(status().isOk())
-            .andExpect(content().json(objectMapper.writeValueAsString(customers)));
+            .andExpect(content().json(objectMapper.writeValueAsString(List.of(customer1, customer2))));
     }
 
     private Customer customer() {
