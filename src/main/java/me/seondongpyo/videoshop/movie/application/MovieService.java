76718de8565309699b98cd70213325621339fa59
seondongpyo <seondongpyo@gmail.com>
2022-06-12 00:35:54 +0900

    feat: 영화 수정 페이지 및 기능

--- src/main/java/me/seondongpyo/videoshop/movie/application/MovieService.java
+++ src/main/java/me/seondongpyo/videoshop/movie/application/MovieService.java
@@ -1,15 +1,14 @@
 package me.seondongpyo.videoshop.movie.application;
 
-import java.util.List;
-import java.util.UUID;
-
-import javax.transaction.Transactional;
-
-import org.springframework.stereotype.Service;
-
 import lombok.RequiredArgsConstructor;
 import me.seondongpyo.videoshop.movie.domain.Movie;
 import me.seondongpyo.videoshop.movie.domain.MovieRepository;
+import me.seondongpyo.videoshop.movie.ui.MovieRequestDTO;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Transactional
@@ -23,12 +22,22 @@ public class MovieService {
 		return movieRepository.save(movie);
 	}
 
+	@Transactional(readOnly = true)
 	public Movie findById(UUID id) {
 		return movieRepository.findById(id)
 			.orElseThrow(IllegalAccessError::new);
 	}
 
+	@Transactional(readOnly = true)
 	public List<Movie> findAll() {
 		return movieRepository.findAll();
+	}
+
+	public void update(UUID id, MovieRequestDTO updateParam) {
+		Movie movie = movieRepository.findById(id)
+			.orElseThrow(IllegalAccessError::new);
+
+		movie.setTitle(updateParam.getTitle());
+		movie.setGenre(updateParam.getGenre());
 	}
 }
--- src/main/java/me/seondongpyo/videoshop/movie/ui/MovieAdminController.java
+++ src/main/java/me/seondongpyo/videoshop/movie/ui/MovieAdminController.java
@@ -7,6 +7,7 @@ import me.seondongpyo.videoshop.movie.domain.Movie;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Arrays;
 import java.util.List;
@@ -54,5 +55,22 @@ public class MovieAdminController {
 	public String add(@ModelAttribute MovieRequestDTO request) {
 		movieService.create(request.toEntity());
 		return "redirect:/admin/movies";
+	}
+
+	@GetMapping("/{id}/edit")
+	public String edit(@PathVariable UUID id, Model model) {
+		Movie movie = movieService.findById(id);
+		model.addAttribute("movie", new MovieResponseDTO(movie));
+		return "admin/movie/edit";
+	}
+
+	@PostMapping("/{id}/edit")
+	public String edit(@PathVariable UUID id,
+					   @ModelAttribute MovieRequestDTO request,
+					   RedirectAttributes redirectAttributes) {
+
+		movieService.update(id, request);
+		redirectAttributes.addAttribute("id", id);
+		return "redirect:/admin/movies/{id}";
 	}
 }
